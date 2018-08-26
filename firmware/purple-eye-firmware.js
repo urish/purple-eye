560abb0b480f493f17ded8f91200ab4d7478d336
Uri Shaked <uri@urishaked.com>
2018-08-27 00:47:31 +0300

    refactor: finish rewriting the firmware in JS

--- firmware/purple-eye-firmware.js
+++ firmware/purple-eye-firmware.js
@@ -1,16 +1,68 @@
 const DEVICE_NAME = 'PurpleEye';
 
-const SERVOS = [D2, D28, D29, D30];
+const SERVOS_PINS = [D29, D30, D2, D28];
 const BATTERY_PIN = D4;
+let servoValues = [0, 0, 0, 0];
+let servoOffsets = [0, 0, 0, 0];
 
+// Simple Task management
+const tasks = {};
+
+function startTask(name, callback, interval) {
+    if (tasks[name] == null) {
+        tasks[name] = setInterval(callback, interval);
+    }
+}
+
+function stopTask(name) {
+    if (tasks[name] != null) {
+        const interval = tasks[name];
+        tasks[name] = null;
+        clearInterval(interval);
+    }
+}
+
+// Battery
 function getBatteryVoltage() {
     return analogRead(BATTERY_PIN) * NRF.getBattery() * 2;
 }
 
-function updateServos(values) {
-    // TODO: calculate pulse length
+function updateBattery() {
+    const percent = Math.max(0, Math.min(100, ((getBatteryVoltage() - 3.6) / 0.6) * 100)) | 0;
+    NRF.updateServices({
+        0x180f: {
+            0x2a19: {
+                value: new Uint8Array([percent]),
+                notify: true,
+            },
+        },
+    });
+}
+
+// Servo
+function updateServos() {
+    for (const i = 0; i < servoValues.length; i++) {
+        if (servoValues[i]) {
+            digitalPulse(SERVOS_PINS[i], HIGH, 1 + servoOffsets[i] + servoValues[i]);
+        }
+    }
+}
+
+function setServoValues(values) {
     for (const i = 0; i < values.length; i++) {
-        analogWrite(SERVOS[i], 0.025, { freq: 20 });
+        // normalize values - between 0 and 1
+        servoValues[i] = values[i] / 180;
+    }
+    if (servoValues.find((v) => v > 0)) {
+        startTask('servo', updateServos, 50);
+    } else {
+        stopTask('servo');
+    }
+}
+
+function setServoOffsets(values) {
+    for (const i = 0; i < values.length; i++) {
+        servoOffsets[i] = values[i];
     }
 }
 
@@ -49,17 +101,18 @@ function onInit() {
             // Servo Control
             0x5100: {
                 0x5200: {
-                    value: new Uint8Array(SERVOS.length),
+                    value: new Uint8Array(SERVOS_PINS.length),
                     description: 'Servo positions',
                     readable: true,
                     writable: true,
-                    onWrite: (evt) => updateServos(new Uint8Array(evt.data)),
+                    onWrite: (evt) => setServoValues(new Uint8Array(evt.data)),
                 },
                 0x5201: {
                     description: 'Servo offsets',
-                    value: new Uint8Array(SERVOS.length),
+                    value: new Uint8Array(SERVOS_PINS.length * 4),
                     readable: true,
                     writable: true,
+                    onWrite: (evt) => setServoOffsets(new Float32Array(evt.data)),
                 },
             },
         },
@@ -68,5 +121,11 @@ function onInit() {
         },
     );
 
-    // TODO sample battery periodically and update battery level service
+    NRF.on('connect', () => {
+        updateBattery();
+        startTask('battery', updateBattery, 10000);
+    });
+    NRF.on('disconnect', () => {
+        stopTask('battery');
+    });
 }
